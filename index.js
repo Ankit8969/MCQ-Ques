16eaa08b51fd0be4741040f49cdda7d74f704e3b
Ankit Kumar <47697582+Ankit8969@users.noreply.github.com>
2021-01-28 15:12:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,52 +22,52 @@ app.get('/', (req, res) => {
 
 app.post('/', async (req, res) => {
     console.log(req.body)
-    // const tempUser = new User()
+    const tempUser = new User()
 
-    // tempUser.name = req.body.name
-    // tempUser.email = req.body.email
+    tempUser.name = req.body.name
+    tempUser.email = req.body.email
 
-    // for (var key in req.body) {
-    //     if (key === "name" || key === "email")
-    //         continue;
+    for (var key in req.body) {
+        if (key === "name" || key === "email")
+            continue;
 
-    //     if (key[0] === 'm') {
-    //         var arr = [];
-    //         if (typeof (req.body[key]) === "string") {
-    //             arr.push(req.body[key])
-    //         }
-    //         else {
-    //             for (var i = 0; i < req.body[key].length; i++) {
-    //                 arr.push(req.body[key][i])
-    //             }
-    //         }
+        if (key[0] === 'm') {
+            var arr = [];
+            if (typeof (req.body[key]) === "string") {
+                arr.push(req.body[key])
+            }
+            else {
+                for (var i = 0; i < req.body[key].length; i++) {
+                    arr.push(req.body[key][i])
+                }
+            }
 
-    //         const temp = {
-    //             number: key.substring(2),
-    //             answer: arr
-    //         }
-    //         tempUser.multipleCorrect.push(temp)
-    //     }
-    //     else if (key[0] === 'p') {
-    //         var arr = [];
-    //         arr.push(req.body[key]);
-    //         const temp = {
-    //             number: key.substring(2),
-    //             answer: arr
-    //         }
-    //         tempUser.paragraph.push(temp)
-    //     }
-    //     else {
-    //         var value = req.body[key];
-    //         const temp = {
-    //             number: key,
-    //             answer: value
-    //         }
-    //         tempUser.singleCorrect.push(temp)
-    //     }
-    // }
-    // await tempUser.save();
-    // res.redirect('/')
+            const temp = {
+                number: key.substring(2),
+                answer: arr
+            }
+            tempUser.multipleCorrect.push(temp)
+        }
+        else if (key[0] === 'p') {
+            var arr = [];
+            arr.push(req.body[key]);
+            const temp = {
+                number: key.substring(2),
+                answer: arr
+            }
+            tempUser.paragraph.push(temp)
+        }
+        else {
+            var value = req.body[key];
+            const temp = {
+                number: key,
+                answer: value
+            }
+            tempUser.singleCorrect.push(temp)
+        }
+    }
+    await tempUser.save();
+    res.redirect('/')
     res.send("Exam Time is over!")
 })
 
